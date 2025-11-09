9882c3b0ddccd5bccce7913dbf694ed0845cb53e
blue-knight99 <gencahmetsaid@gmail.com>
2025-11-09 17:36:43 +0300

    Login page is created. db_helper is created

---
+++ db_helper.py
@@ -0,0 +1,51 @@
+import mysql.connector
+
+class DatabaseHandler:
+    def __init__(self,host="localhost",user="root",password="",database="classicmodels"):
+        self.db = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        self.cursor = self.db.cursor(dictionary=True) # returns results as dict
+
+    def check_customer_number(self,num):
+        """
+        check if the customer number exits in customer table
+        """
+        query = "SELECT * FROM customers WHERE customerNumber = %s"
+        self.cursor.execute(query,(num,))
+        result = self.cursor.fetchone()
+        return result
+    
+    def execute_query(self,query, params=None):
+        """
+        execute the given query
+        Returns:
+            - For Select: fetched rows
+            - For Insert/Update/Delete: number of affected rows
+        Raises:
+            - mysql.connector.Error if the query fails
+        """
+        try:
+            if params:
+                self.cursor.execute(query, params)
+            else:
+                self.cursor.execute(query)
+
+            # if its a select query
+            if query.strip().lower().startswith("select"):
+                result = self.cursor.fetchall()
+                return result 
+            else:
+                self.db.commit() # commit changes for insert/update/delete
+                return self.cursor.rowcount
+        
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
+    
+    def close(self):
+        self.cursor.close()
+        self.db.close()
--- server.py
+++ server.py
@@ -1,21 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from db_helper import DatabaseHandler
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey" # for flash messages
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="", # Enter your SQL password here
-    database="classicmodels"
-)
+db = DatabaseHandler(password="") # enter password
 
 @app.route('/')
 def index():
-    cursor = db.cursor(dictionary=True)
-    cursor.execute("SELECT productLine, textDescription FROM productlines LIMIT 2")
-    productlines = cursor.fetchall()
+    query = ("SELECT productLine, textDescription FROM productlines LIMIT 2")
+    productlines = db.execute_query(query)
     return render_template('index.html', productlines=productlines)
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        number_input = request.form.get("number")
+        if not number_input.isdigit():
+            flash("Please enter a valid number!", "danger")
+            return redirect(url_for("login"))
+
+        number_input = int(number_input)
+        customer = db.check_customer_number(number_input)
+        if customer:
+            # store customer info in session
+            session["customer_number"] = customer["customerNumber"]
+            flash("Login successful!", "success")
+            return index()
+        else:
+            flash("Number not found!", "danger")
+
+        return redirect(url_for("login"))
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("You have been logged out","info")
+    return redirect(url_for("login"))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
